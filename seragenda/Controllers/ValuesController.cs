5d0da1c0ffef1e80a12d1f7f982a6305d046308d
johan <166138951+DragX02@users.noreply.github.com>
2026-02-05 17:09:16 +0100

    Ajoutez des fichiers projet.

---
+++ seragenda/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using Dapper;
+using seragenda.Models;
+
+namespace seragenda.Controllers
+{
+
+    [Route("api/[controller")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public ValuesController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        public IActionResult GetCours()
+        {
+            var ConString = _config.GetConnectionString("DefaultConnection");
+            using (var connection = new NpgsqlConnection(ConString))
+            {
+                var sql = " SELECT * FROM cours";
+                var maList = connection.Query<Cours>(sql).ToList();
+                return Ok(maList);
+            }
+        }
+    }
+
+
+}   
---
+++ seragenda/Models/Cours.cs
@@ -0,0 +1,8 @@
+﻿namespace seragenda.Models
+{
+    public class Cours
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+    }
+}
